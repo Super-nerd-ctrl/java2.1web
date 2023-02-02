b1e3b677854d02445ff866c59121e59f33112002
tong <1390855466@qq.com>
2023-02-02 14:55:27 +0800

    增加了ResponseServlet，把对象转化为JSON字符串响应给浏览器

---
+++ javaweb/src/main/java/com/yt/javaweb/ResponseServlet.java
@@ -0,0 +1,32 @@
+package com.yt.javaweb;
+
+import com.alibaba.fastjson.JSON;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ResponseServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String userName = req.getParameter("userName");
+        System.out.println("userName = " + userName);
+        String password = req.getParameter("password");
+        System.out.println("password = " + password);
+
+        resp.setContentType("text/json;charset=UTF-8");
+        User user = new User();
+        user.setAge(18);
+        user.setUsername("哈哈哈");
+        user.setPassword("123456");
+
+        String s = JSON.toJSONString(user);
+        PrintWriter writer = resp.getWriter();
+        writer.println(s);
+    }
+}
---
+++ javaweb/src/main/java/com/yt/javaweb/User.java
@@ -0,0 +1,13 @@
+package com.yt.javaweb;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+
+    private String password;
+
+    private int age;
+}
